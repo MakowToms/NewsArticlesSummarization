893c6b334c43da15ae07437f18e1a1a8062d4b0c
Pastuszka <michal.pastuszka98@gmail.com>
2022-04-27 19:45:47 +0200

    add subjectivity filtering

---
+++ subjectivity/score_subjectivity.py
@@ -0,0 +1,72 @@
+import sys
+import json
+from transformers import pipeline
+import nltk
+nltk.download('punkt')
+
+PATH = 'data/newsroom/sample-v1.json'
+
+
+class TransformerSentiment:
+    def __init__(self, task='sentiment-analysis'):
+        self.pipeline = pipeline(task)
+
+    def score(self, sentences):
+        sentiments = self.pipeline(sentences)
+        return [1-sentiment['score'] for sentiment in sentiments]
+
+
+def score_transformer(file):
+
+    sentiment_pipeline = pipeline("sentiment-analysis")
+    all_texts = []
+    all_summaries = []
+    all_sentences = []
+    all_scores = []
+    for row in text_json:
+        sentences = nltk.tokenize.sent_tokenize(row['text'])
+        sentiments = sentiment_pipeline(sentences)
+        scores = [1-sentiment['score'] for sentiment in sentiments]
+        all_texts.append(row['text'])
+        all_sentences.append(sentences)
+        all_scores.append(scores)
+        all_summaries.append(row['summary'])
+
+
+def load_json_data(path):
+    with open(path) as f:
+        text_json = json.load(f)
+    texts = [i['text'] for i in text_json]
+    summaries = [i['summary'] for i in text_json]
+    return texts, summaries
+
+
+def save_json_data(path, texts, summaries, scores, sentences):
+    split_path = path.split('.')
+    save_path = split_path[0] + '_subj_scored.' + split_path[1]
+
+    with open(save_path, "w") as f:
+        json.dump([{
+            "text": text,
+            "summary": summary,
+            "scores": score,
+            "sentences": sentence,
+        } for text, summary, score, sentence in zip(texts, summaries, scores, sentences)], f, indent=4)
+
+
+def main():
+    texts, summaries = load_json_data(PATH)
+    scorer = TransformerSentiment()
+    all_sentences = []
+    all_scores = []
+    for text in texts:
+        sentences = nltk.tokenize.sent_tokenize(text)
+        scores = scorer.score(sentences)
+        all_sentences.append(sentences)
+        all_scores.append(scores)
+    save_json_data(PATH, texts, summaries, all_scores, all_sentences)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
