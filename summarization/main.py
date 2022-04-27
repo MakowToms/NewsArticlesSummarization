8b339aeaf803e91d63bc9e43cf238d65ca9137cb
MakowToms <tomasz.tu.tm@gmail.com>
2022-04-27 17:54:03 +0200

    add sample newsroom data

--- summarization/main.py
+++ summarization/main.py
@@ -6,13 +6,21 @@ from transformers import PegasusForConditionalGeneration, PegasusTokenizer, Seq2
 from summarization.data import load_newsroom
 
 
+def filter_texts(texts):
+    return [
+        txt.replace("\n", "").replace("\x97", "") for txt in texts
+    ]
+
 newsroom = load_newsroom(subset=True)
 # multi_news = load_dataset("multi_news")
 # "google/pegasus-large" - selects most important sentences from text
-samples = 32
+samples = 100
 model_name = "google/pegasus-xsum"
 device = "cpu"
-src_texts = [str(text) for text in newsroom.data["train"]["text"][:samples]]
+src_texts = filter_texts([str(text) for text in newsroom.data["train"]["text"][:samples]])
+target_texts = filter_texts([str(text) for text in newsroom.data["train"]["summary"][:samples]])
+mean_len = sum([len(t) for t in src_texts]) / len(src_texts)
+
 
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
@@ -23,11 +31,11 @@ translated = model.generate(**batch)
 tgt_texts = tokenizer.batch_decode(translated, skip_special_tokens=True)
 
 
-with open("data/example.json", "w") as f:
+with open("data/sample-v1.json", "w") as f:
     json.dump([{
         "text": text,
-        "pred_summary": pred
-    } for text, pred in zip(src_texts, tgt_texts[:samples])], f, indent=4)
+        "summary": pred
+    } for text, pred in zip(src_texts, target_texts)], f, indent=4)
 
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
