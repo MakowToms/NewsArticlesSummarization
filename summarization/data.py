ca853a678665807f774d04841d81442a2539b57e
MakowToms <tomasz.tu.tm@gmail.com>
2022-04-27 18:03:13 +0200

    refactor getting sample data

--- summarization/data.py
+++ summarization/data.py
@@ -1,4 +1,5 @@
-import torch
+import json
+
 from datasets import load_dataset, DatasetDict
 
 # force not using cuda
@@ -25,3 +26,27 @@ def load_newsroom(subset=True):
     else:
         dataset = load_dataset("newsroom", data_dir="data/newsroom")
     return dataset
+
+
+def filter_texts(texts):
+    return [
+        txt.replace("\n", "").replace("\x97", "") for txt in texts
+    ]
+
+
+def save_sample_data(file_name="sample-v1"):
+    newsroom = load_newsroom(subset=True)
+    samples = 100
+    src_texts = filter_texts([str(text) for text in newsroom.data["train"]["text"][:samples]])
+    target_texts = filter_texts([str(text) for text in newsroom.data["train"]["summary"][:samples]])
+
+    mean_len = sum([len(t) for t in src_texts]) / samples
+    mean_sum = sum([len(t) for t in target_texts]) / samples
+    print(f"Mean text length: {mean_len}")
+    print(f"Mean summary length: {mean_sum}")
+
+    with open(f"data/newsroom/{file_name}.json", "w") as f:
+        json.dump([{
+            "text": text,
+            "summary": pred
+        } for text, pred in zip(src_texts, target_texts)], f, indent=4)
--- summarization/main.py
+++ summarization/main.py
@@ -6,21 +6,11 @@ from transformers import PegasusForConditionalGeneration, PegasusTokenizer, Seq2
 from summarization.data import load_newsroom
 
 
-def filter_texts(texts):
-    return [
-        txt.replace("\n", "").replace("\x97", "") for txt in texts
-    ]
-
-newsroom = load_newsroom(subset=True)
 # multi_news = load_dataset("multi_news")
 # "google/pegasus-large" - selects most important sentences from text
-samples = 100
+
 model_name = "google/pegasus-xsum"
 device = "cpu"
-src_texts = filter_texts([str(text) for text in newsroom.data["train"]["text"][:samples]])
-target_texts = filter_texts([str(text) for text in newsroom.data["train"]["summary"][:samples]])
-mean_len = sum([len(t) for t in src_texts]) / len(src_texts)
-
 
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
@@ -31,11 +21,6 @@ translated = model.generate(**batch)
 tgt_texts = tokenizer.batch_decode(translated, skip_special_tokens=True)
 
 
-with open("data/sample-v1.json", "w") as f:
-    json.dump([{
-        "text": text,
-        "summary": pred
-    } for text, pred in zip(src_texts, target_texts)], f, indent=4)
 
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
