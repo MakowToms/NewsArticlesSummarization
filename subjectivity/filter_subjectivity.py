fd69d983bc88c11f17772dbf10704c892f18d7b2
Pastuszka <michal.pastuszka98@gmail.com>
2022-05-18 11:40:25 +0200

    adjust filter_subjectivity.py to new format

--- subjectivity/filter_subjectivity.py
+++ subjectivity/filter_subjectivity.py
@@ -5,7 +5,7 @@ import numpy as np
 def load_json_scored(path):
     with open(path) as f:
         text_json = json.load(f)
-    texts = [i['text'] for i in text_json]
+    texts = [i['texts'] for i in text_json]
     summaries = [i['summary'] for i in text_json]
     sentences = [i['sentences'] for i in text_json]
     scores = [i['scores'] for i in text_json]
@@ -15,10 +15,13 @@ def load_json_scored(path):
 def load_filtered(path, threshold=0.001):
     texts, summaries, all_sentences, all_scores = load_json_scored(path)
     filtered_texts = []
-    for sentences, scores in zip(all_sentences, all_scores):
-        sentences_np = np.array(sentences)
-        filtered_sentences = sentences_np[
-            np.where(np.array(scores, dtype=float) > threshold)
-        ]
-        filtered_texts.append(' '.join(filtered_sentences))
+    for sentences_list, scores_list in zip(all_sentences, all_scores):
+        filtered_text_list = []
+        for sentences, scores in zip(sentences_list, scores_list):
+            sentences_np = np.array(sentences)
+            filtered_sentences = sentences_np[
+                np.where(np.array(scores, dtype=float) > threshold)
+            ]
+            filtered_text_list.append(' '.join(filtered_sentences))
+        filtered_texts.append(filtered_text_list)
     return texts, summaries, filtered_texts
