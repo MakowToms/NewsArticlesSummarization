39032cf7b37792b20e1bf51d88a638d2e679f803
MakowToms <tomasz.tu.tm@gmail.com>
2022-05-24 15:41:49 +0200

    add results comparison

--- compare_results.py
+++ compare_results.py
@@ -3,12 +3,14 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-RESULTS_DIR = Path("results/pegasus/")
+RESULTS_DIR = Path("results/")
+CSV_NAME = "news-summarization-NLP.csv"
 
 
 def get_data_df():
     data = pd.concat([
-        pd.read_csv(RESULTS_DIR / "news-summarization-NLP.csv"),
+        pd.read_csv(RESULTS_DIR / "pegasus" / CSV_NAME),
+        pd.read_csv(RESULTS_DIR / "bert_ext" / CSV_NAME),
     ])
     data = data.drop_duplicates()
     data["is_blob"] = data["input_json"].apply(lambda x: x.endswith('blob.json'))
@@ -17,7 +19,8 @@ def get_data_df():
 
 
 DF = get_data_df()
-DF.columns
+DF = DF[DF["Id"].apply(lambda x: x not in ["NEWS-4", "NEWS-121", "NEWS-127"])]
+
 
 def plot_one(data, filter_type, plot_type="rouge1"):
     filtered = data[(data["random"] == False) & (data["filtered"] == True)]
@@ -67,6 +70,12 @@ def plot(is_blob, is_multi, method, title, filename):
 
 plot(True, True, "Pegasus", "Pegasus with multinews", "pegasus_multi_blob")
 plot(False, True, "Pegasus", "Pegasus with multinews", "pegasus_multi_transformer")
+plot(True, False, "Pegasus", "Pegasus with newsroom", "pegasus_room_blob")
+plot(False, False, "Pegasus", "Pegasus with newsroom", "pegasus_room_transformer")
+plot(True, True, "BertExt", "BertExt with multinews", "bertExt_multi_blob")
+plot(False, True, "BertExt", "BertExt with multinews", "bertExt_multi_transformer")
+plot(True, False, "BertExt", "BertExt with newsroom", "bertExt_room_blob")
+plot(False, False, "BertExt", "BertExt with newsroom", "bertExt_room_transformer")
 
 
 def plot2_one(data, plot_type="rouge1"):
@@ -92,7 +101,7 @@ def plot2(is_multi, method, title, filename):
     df2["filtered_l"] = df2['text_len']
     data[data["filtered"] == False] = df2
 
-    data = data.sort_values("threshold")
+    data = data.sort_values("filtered_l")
 
     plt.subplots(1, 3, figsize=(24, 6))
     plt.subplot(1, 3, 1)
@@ -113,3 +122,6 @@ def plot2(is_multi, method, title, filename):
 
 
 plot2(True, "Pegasus", "Pegasus with multinews", "pegasus_multi")
+plot2(False, "Pegasus", "Pegasus with newsroom", "pegasus_room")
+plot2(True, "BertExt", "BertExt with multinews", "bertExt_multi")
+plot2(False, "BertExt", "BertExt with newsroom", "bertExt_room")
